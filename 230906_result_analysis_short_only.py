ba4e8088edd79fc696656e8bdb1a3e53cea79017
worldest0628@gmail.com <worldest0628@gmail.com>
2023-09-06 20:25:14 +0900

    //230906_simulation_result_0.001, 0.009, 0.001_2, 20, 1.xlsx 파일 분석내용 //n이 커질 수록 수익이 커지는 건 의미가 없음 -> n 을 키웠는데, 딱 그 범위에 값이 걸렸을 대 수익이 크게 나오는 거임. 그래서 n이 작을 수록 분포가 매끈하고 n 이 클 수 록 ^^^^가 심한거임 //n이 커지는 방향으로 값이 기울기가 saturation 되는 부분으로 보이는 6 또는 8부근 이 n의 최적으로 보임 //a percent의 경우, 230906_simulation_result_0.001, 0.009, 0.001_2, 20, 1  파일 오른쪽에 sumifs로 분석해봄 -> 0.002 ~ 0.003 이 최적으로 보임. 이를 더 자세히 분석하기 위해서 0.002~0.003 사이를 잘게 쪼갠 시뮬레이션 돌리는 중

---
+++ 230906_result_analysis_short_only.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+# Load data from the CSV file
+# df = pd.read_csv('230906_simulation_result_0.001, 0.009, 0.001_2, 20, 1.csv')
+df = pd.read_excel('230906_simulation_result_0.001, 0.009, 0.001_2, 20, 1.xlsx')
+
+# Extract data from columns
+x_data = df['a']
+y_data = df['n']
+z_data = df['profit']
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Create the surface plot directly with x, y, and z data
+surf = ax.plot_trisurf(x_data, y_data, z_data, cmap='viridis')
+
+# Set labels for each axis
+ax.set_xlabel('Position liquidation percent')
+ax.set_ylabel('Position grid')
+ax.set_zlabel('Profit')
+ax.set_title('Profit Simulation')
+
+# Show the plot
+plt.show()
